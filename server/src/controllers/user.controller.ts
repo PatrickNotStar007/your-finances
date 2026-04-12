0bd9b735bdef1ebd7ad1a1be275327482398010a
Patrick <h.ilya2003@mail.ru>
2026-04-12 10:45:12 +0300

    add erorrs handlers

--- server/src/controllers/user.controller.ts
+++ server/src/controllers/user.controller.ts
@@ -1,19 +1,22 @@
-import { type Request, type Response } from 'express'
+import type { Request, Response } from 'express'
 import { Prisma } from '../generated/prisma/client'
 import { prisma } from '../utils/prisma.service'
 
 export const userController = {
     async create(req: Request, res: Response) {
         try {
-            const { name, email, password } = req.body
+            const { name, email, passwordHash } = req.body
 
-            if (!name || !email || !password)
-                throw new Error('Поля name и email обязательны')
+            if (!name || !email || !passwordHash)
+                return res
+                    .status(401)
+                    .json({ error: 'Поля name, email и password обязательны' })
 
             const user = await prisma.user.create({ data: req.body })
-            res.status(201).json(user)
+            res.status(200).json(user)
         } catch (e) {
-            console.log(e)
+            console.error('Ошибка при создании пользователя: ', e)
+            res.status(500).json({ error: 'Не удалось создать пользователя' })
         }
     },
 
